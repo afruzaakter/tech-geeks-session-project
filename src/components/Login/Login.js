021833dd05d29bb6f04a5c9de0e0202b6ff3f5ea
afruzaakter <afruzaaktersonia1995@gmail.com>
2022-04-08 12:31:56 +0600

    update login button

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,52 @@
-import React from "react";
-
+// import React, { useState } from "react";
+import './Login.css';
+import GoogleLogo  from '../../Assets/Image/google.svg';
 const Login = () => {
+
+
+
+
+
   return (
-    <div>
-      <h1>This is login page</h1>
+    <div className="auth-form-container d-flex justify-content-center align-items-center ">
+     <div className="auth-form ">
+       <h1>Login</h1>
+       <form>
+           <div className="input-field">
+             <label htmlFor="email">Email</label>
+               <div className="input-wrapper">
+               <input type="text" name="email" id="email" placeholder="Email" required/>
+             </div>
+           </div>
+           <div className="input-field">
+             <label htmlFor="password">Password</label>
+               <div className="input-wrapper">
+               <input type="password" name="password" id="password" placeholder="Password" required/>
+             </div>
+           </div>
+           <button type="submit" className="auth-form-submit">Login</button>
+       </form>
+      {/* ///////////////////////// */}
+
+      <p className="redirect">
+        New to Tech Geeks?{" "}
+        <span>Create New Account</span>
+      </p>
+    <div className="horizontal-divider">
+      <div className="line-left"></div>
+        <p>or</p>
+        <div className="line-right" >
+      </div>
+    </div>
+   <div className="input-wrapper">
+     <button className="google-auth">
+       <img src={GoogleLogo} alt="" />
+       <p>Continue with Google</p>
+     </button>
+   </div>
+
+
+     </div>
     </div>
   );
 };
